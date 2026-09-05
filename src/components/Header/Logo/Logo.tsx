69afa907682bf661b25b139e4a44d45cf967cbf2
AlexMurev <alemurovalex@yandex.ru>
2026-09-06 01:29:29 +0500

    add favicon

--- src/components/Header/Logo/Logo.tsx
+++ src/components/Header/Logo/Logo.tsx
@@ -3,7 +3,9 @@ import "./Logo.css";
 const Logo = () => {
     return (
         <div className="logo-container">
-            <div className="logo__box"></div>
+            <div className="logo__box">
+                <img src={`${import.meta.env.BASE_URL}favicon.png`}/>
+            </div>
             <div>
                 <div className="logo__title">VoxelLab</div>
                 <div className="logo__sub">Modding tools for VoxelCore</div>
