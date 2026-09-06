ca2c20b9d89861aebb8170290c197c6972e49d35
AlexMurev <alemurovalex@yandex.ru>
2026-09-06 18:55:35 +0500

    update vec3input styles

--- src/components/Vector3Input/Vector3Input.tsx
+++ src/components/Vector3Input/Vector3Input.tsx
@@ -10,7 +10,6 @@ interface Vector3InputProps {
 const Vector3Input = ({ label, placeholders = ["0", "0", "0"], value, onChange }: Vector3InputProps) => {
     const handleChange = (index: "x" | "y" | "z", textValue: string) => {
         const numValue = textValue === "" ? 0 : parseFloat(textValue);
-
         if (!isNaN(numValue)) {
             const newVector = value.clone();
             newVector[index] = numValue;
@@ -19,32 +18,32 @@ const Vector3Input = ({ label, placeholders = ["0", "0", "0"], value, onChange }
     };
 
     return (
-        <div className="sidebar__field">
-            <label className="sidebar__label">{label}</label>
-            <div className="sidebar__transform-group">
-                <div className="sidebar__input-wrapper">
-                    <span className="sidebar__axis-label sidebar__axis-label--x">X</span>
+        <div className="vector3-input__field">
+            <label className="vector3-input__label">{label}</label>
+            <div className="vector3-input__group">
+                <div className="vector3-input__wrapper">
+                    <span className="vector3-input__axis-label vector3-input__axis-label--x">X</span>
                     <input
                         type="number"
-                        className="sidebar__input"
+                        className="vector3-input__input"
                         placeholder={placeholders[0]}
                         value={value["x"]}
                         onChange={(e) => handleChange("x", e.target.value)}
                     />
                 </div>
-                <div className="sidebar__input-wrapper">
-                    <span className="sidebar__axis-label sidebar__axis-label--y">Y</span>
+                <div className="vector3-input__wrapper">
+                    <span className="vector3-input__axis-label vector3-input__axis-label--y">Y</span>
                     <input
-                        className="sidebar__input"
+                        className="vector3-input__input"
                         placeholder={placeholders[1]}
                         value={value["y"]}
                         onChange={(e) => handleChange("y", e.target.value)}
                     />
                 </div>
-                <div className="sidebar__input-wrapper">
-                    <span className="sidebar__axis-label sidebar__axis-label--z">Z</span>
+                <div className="vector3-input__wrapper">
+                    <span className="vector3-input__axis-label vector3-input__axis-label--z">Z</span>
                     <input
-                        className="sidebar__input"
+                        className="vector3-input__input"
                         placeholder={placeholders[2]}
                         value={value["z"]}
                         onChange={(e) => handleChange("z", e.target.value)}
